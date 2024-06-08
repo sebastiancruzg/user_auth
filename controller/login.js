ca158be8ddbc7c0d955c4c47e85f83422ad4c9e7
Sebastian Gutierrez <desarrolloctic10@ttncompany.com>
2024-06-08 15:11:55 -0500

    feat: create login

--- controller/login.js
+++ controller/login.js
@@ -1,6 +1,24 @@
+import { UserModel } from '../models/index.js'
+import bcrypt from 'bcrypt'
 
 export class LoginController {
   static log = async (req, res) => {
-    res.send('everything good')
+    try {
+      const { username, password } = req.body
+      const validateUser = await UserModel.getAll({ username })
+      if (validateUser.rowCount === 0) {
+        return res.status(409).send('user not found')
+      }
+
+      const storePassword = validateUser.rows[0].password
+      const isCorrectPassword = await bcrypt.compare(password, storePassword)
+      if (!isCorrectPassword) {
+        return res.status(400).send('wrong password')
+      }
+      res.send('Welcome').status(202)
+    } catch (error) {
+      res.send('internal server error').status(500)
+      throw new Error(error)
+    }
   }
 }
